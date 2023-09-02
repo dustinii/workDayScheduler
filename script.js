d08ce3d29caa9eb937cf391bd3a58b2e80094d7c
dustinii <125335987+dustinii@users.noreply.github.com>
2023-09-01 23:38:59 -0700

    added to index.html and cleaned js

--- script.js
+++ script.js
@@ -1,58 +1,45 @@
-
 $(function () {
   displayCurrentDate();
-  loadTimeBlockDescriptions();
-
-  $(".saveBtn").on("click", saveDescription);
-
-  updateTimeBlockColors(); // Add this line to call the function when the DOM is ready
-
-  setInterval(updateTimeBlockColors, 60000); // Update the colors every minute
-});
-
-function updateTimeBlockColors() {
-  var currentHour = dayjs().hour();
-
-  $(".time-block").each(function () {
-    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
-
-    if (timeBlockHour < currentHour) {
-      $(this).addClass("past").removeClass("present future");
-    } else if (timeBlockHour === currentHour) {
-      $(this).addClass("present").removeClass("past future");
-    } else {
-      $(this).addClass("future").removeClass("past present");
-    }
-  });
-}
-
-// Function to load saved events from localStorage
-function loadSavedEvents() {
-  $(".time-block").each(function () {
-    var timeBlockId = $(this).attr("id");
-    var savedEvent = localStorage.getItem(timeBlockId);
-
-    if (savedEvent) {
-      $(this).children(".description").val(savedEvent);
-    }
-  });
-}
-
-// Wrap all code that interacts with the DOM in a call to jQuery
-$(document).ready(function () {
-  // Display the current date
-  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
-
-  // Save button click event
-  $(".saveBtn").on("click", function () {
-    var textAreaValue = $(this).siblings(".description").val();
-    var timeBlockId = $(this).parent().attr("id");
-    localStorage.setItem(timeBlockId, textAreaValue);
-  });
-
-  // Update the time block colors
+  loadSavedEvents();
   updateTimeBlockColors();
 
-  // Load saved events
-  loadSavedEvents();
+  $(".saveBtn").on("click", saveDescription);
+  setInterval(updateTimeBlockColors, 60000); // Refreshes time block colors every minute
 });
+
+function displayCurrentDate() {
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+}
+
+function updateTimeBlockColors() {
+  var currentHour = 10;
+
+  $(".time-block").each(function () {
+      var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      if (timeBlockHour < currentHour) {
+          $(this).addClass("past").removeClass("present future");
+      } else if (timeBlockHour === currentHour) {
+          $(this).addClass("present").removeClass("past future");
+      } else {
+          $(this).addClass("future").removeClass("past present");
+      }
+  });
+}
+
+function loadSavedEvents() {
+  $(".time-block").each(function () {
+      var timeBlockId = $(this).attr("id");
+      var savedEvent = localStorage.getItem(timeBlockId);
+
+      if (savedEvent) {
+          $(this).children(".description").val(savedEvent);
+      }
+  });
+}
+
+function saveDescription() {
+  var textAreaValue = $(this).siblings(".description").val();
+  var timeBlockId = $(this).parent().attr("id");
+  localStorage.setItem(timeBlockId, textAreaValue);
+}
